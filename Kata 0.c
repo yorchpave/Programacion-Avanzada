4b17f544af9a50f3fd05e9cf62d16685ac4650f9
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-08-17 18:50:05 -0500

    Create Kata 0.c

---
+++ Kata 0.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *getIndexes(int *nums, int len, int target)
+{
+    int i, j;
+    int candidate;
+    int complement;
+    int *result = (int *)calloc(2, sizeof(int));
+
+    for (i = 0; i < len - 1; i++)
+    {
+        if (nums[i] >= target)
+        {
+            continue;
+        }
+        candidate = nums[i];
+        complement = target - candidate;
+        for (j = i + 1; j < len; j++)
+        {
+            if (nums[j] == complement)
+            {
+                break;
+            }
+        }
+        if (nums[i] + nums[j] == target)
+        {
+            result[0] = i;
+            result[1] = j;
+            return result;
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    int nums[] = {2, 7, 11, 15};
+    int len = 4;
+    int target = 9;
+    int *result = getIndexes(nums, len, target);
+    printf("");
+    if (result != NULL && (nums[result[0]] + nums[result[1]] == target))
+    {
+        printf("Works!!\n");
+    }
+    else
+    {
+        printf(":(\n");
+    }
+}
