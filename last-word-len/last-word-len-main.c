6765cfb39fa0aed0f9a66d61a1eb0632c0d2fac1
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-09-04 12:14:15 -0500

    Update last-word-len-main.c

--- last-word-len/last-word-len-main.c
+++ last-word-len/last-word-len-main.c
@@ -3,7 +3,7 @@
 #include "last_word_len.h"
 
 int main(int argc, char **argv) {
-    //char * phrase = "Hola Mundo como estamos todos";
+    //char * phrase = "Hola mundo como estamos hoy";
     char * phrase = "Hola";
     int len = last_word_len(phrase, 4);
     printf("De la frase: \"%s\" la última palabra tiene longitud: %d\n", phrase, len);
