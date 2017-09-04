a321e64638be19ad8f697b228b2ee0fc46768ca7
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-09-04 12:04:29 -0500

    Update two-sum-main.c

--- two-sum/two-sum-main.c
+++ two-sum/two-sum-main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include "two-sum.h"
+#include <stdlib.h>
+#include "two_sum.h"
 
-int main()
-{
-    int nums[] = {2, 7, 11, 15};
-    int len = 4;
-    int target = 9;
-    int *result = getIndexes(nums, len, target);
-    printf("");
-    if (result != NULL && (nums[result[0]] + nums[result[1]] == target))
-    {
-        printf("Works!!\n");
+int main (int argc, char ** argv) {
+    int nums[] = {-1, -2, -3, -4, -5};
+    int target = -8, num_size = 5;
+    int *result = two_sum(nums, num_size, target);
+    if(result != NULL && nums[result[0]] + nums[result[1]] == target) {
+        printf("[%d, %d] = %d \n", result[0], result[1], nums[result[0]] + nums[result[1]]);
+        printf("It was true! \n");
+    } else {
+        printf("It lied! The MACHINE lied! \n");
     }
-    else
-    {
-        printf(":(\n");
-    }
+    free(result);
+    return 0;
 }
