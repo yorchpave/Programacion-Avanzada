bf5cd461ab36d7bce3f06f4bae2671fddf578946
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-10-12 18:43:28 -0500

    Create island-perimeter.c

---
+++ island-perimeter/island-perimeter.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "island_perimeter.h"
+
+int island_perimeter(int ** island, int row_size, int col_size) {
+	int perimeter = 0, i, j, temp_perimeter;
+	for (i = 0; i < row_size; i++) {
+		for (j = 0; j < col_size; j++) {
+			temp_perimeter = 4;
+			if (island[i][j] == 0) {
+				temp_perimeter = 0;
+			}
+			else {
+				if (i != 0) {
+					temp_perimeter -= island[i - 1][j];
+				}
+				if (i != row_size - 1) {
+					temp_perimeter -= island[i + 1][j];
+				}
+				if (j != 0) {
+					temp_perimeter -= island[i][j - 1];
+				}
+				if (j != col_size - 1) {
+					temp_perimeter -= island[i][j + 1];
+				}
+			}
+			perimeter += temp_perimeter;
+		}
+	}
+
+	return perimeter;
+}
