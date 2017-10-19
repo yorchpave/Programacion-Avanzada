1610ea109e56846d62cf8e16c63ce4d538c6e3b2
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-10-19 12:53:49 -0500

    Create test_add-digits.c

---
+++ mini_proyecto_2/test_add-digits.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "minunit.h"
+#include "add-digits.h"
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
+
+int testsRun = 0;
+
+static char * testUnit() {
+  int number = 1865;
+  int result = addDigits(number);
+  int expected = 2;
+  muAssert("Number = 1865, expected digit sum = 2", result == expected);
+  return 0;
+}
+
+static char * allTests() {
+  muRunTest(testUnit);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  char *result = allTests();
+  if (result != 0) {
+    printf("-_-_-_-_-_-_-_,------,      o \n");
+    printf("_-_-_-_-_-_-_-|   /\\_/\\ \n");
+    printf("-_-_-_-_-_-_-~|__( X .X)  +     + \n");
+    printf("_-_-_-_-_-_-_- \"\"  \"\" \n");
+    printf(KRED "✗ %s \n" RESET, result);
+  }
+  else {
+    printf("-_-_-_-_-_-_-_,------,      o \n");
+    printf("_-_-_-_-_-_-_-|   /\\_/\\ \n");
+    printf("-_-_-_-_-_-_-~|__( ^ .^)  +     + \n");
+    printf("_-_-_-_-_-_-_-  \"\"  \"\" \n");
+    printf(KGRN " ✓ ALL TESTS PASSED \n" RESET);
+  }
+  printf("Tests run: %d\n", testsRun);
+  return result != 0;
+}
