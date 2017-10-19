1d9ba79b806842f1aa99fcdcc7d0b0a01441cbda
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-10-19 13:25:30 -0500

    Update add-digits.c

--- mini_proyecto_2/add-digits.c
+++ mini_proyecto_2/add-digits.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include "add-digits.h"
 
-int addDigits(unsigned int n){
-  return 1 + ((n - 1) % 9);
+int addDigits(int num){
+  if (num == 0) 
+    return 0; 
+  if (num % 9 == 0) 
+    return 9; 
+  else 
+     return num % 9;
 }
