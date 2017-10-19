20ad11ac0cdc9bd1d53cd663d6e4a954f5957120
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-10-19 12:56:34 -0500

    Create main.c

---
+++ mini_proyecto_2/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "add-digits.h"
+
+Add function signatures here
+
+int main(int argc, char **argv) {
+
+  printf("Hello from main\n");
+  return 0;
+}
