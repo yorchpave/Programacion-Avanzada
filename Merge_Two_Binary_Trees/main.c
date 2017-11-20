ba256ecbce61dca8b10774309f5829ad59349d5c
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2017-11-20 16:40:37 -0600

    Create main.c

---
+++ Merge_Two_Binary_Trees/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "merge_trees.h"
+
+int main(int argc, char **argv) {
+    /* Start your code here */
+    printf("Hello from main\n");
+    return 0;
+}
